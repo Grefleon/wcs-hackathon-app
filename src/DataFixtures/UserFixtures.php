63b06895c0771a33d0f631480680e5a4b2c2abb6
Tobe Continued <do.you.continue@gmail.com>
2020-06-24 22:21:43 +0200

    formulaire d'identification user et entity user

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends Fixture
+{
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setUsername('Billy');
+        $user->setPassword($this->passwordEncoder->encodePassword($user,'password'));
+        $user->setEmail('billy45000@gmail.com');
+        $user->setAvatar();
+        $user->setExperience();
+        $user->setLevel();
+
+        $manager->persist($user);
+
+        $manager->flush();
+    }
+}
