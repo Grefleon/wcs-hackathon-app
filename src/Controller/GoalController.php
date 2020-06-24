093caab57a787e8677e5eaa47dfd5a63d00a08ed
Olivier Monsire <olivier.monsire@gmail.com>
2020-06-25 00:54:31 +0200

    add goal card

--- src/Controller/GoalController.php
+++ src/Controller/GoalController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Goal;
+use App\Repository\GoalRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -13,8 +15,13 @@ class GoalController extends AbstractController
 {
     /**
      * @Route("/", name="goal_index")
+     * @param GoalRepository $goals
+     * @return Response
      */
-    public function index () {
-        return $this->render('goal/index.html.twig');
+    public function index (GoalRepository $goals) {
+
+        return $this->render('goal/index.html.twig', [
+            'goals'=>$goals->findAll()
+        ]);
     }
 }
