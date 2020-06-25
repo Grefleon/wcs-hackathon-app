4764133314f72a3021f0de2f99c7efc443cd79e1
Tobe Continued <do.you.continue@gmail.com>
2020-06-25 11:10:45 +0200

    User Fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,7 +7,6 @@
 const $ = require('jquery');
 // any CSS you import will output into a single css file (app.css in this case)
 import '../scss/app.scss';
-import '../scss/home.scss';
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
 require('bootstrap');
