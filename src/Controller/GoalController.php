c68b832db7ef5fb90ee73ad5a839840501520bba
Olivier Monsire <olivier.monsire@gmail.com>
2020-06-25 19:27:20 +0200

    modify index

--- src/Controller/GoalController.php
+++ src/Controller/GoalController.php
@@ -3,28 +3,48 @@
 namespace App\Controller;
 
 use App\Entity\Goal;
+use App\Entity\User;
 use App\Repository\GoalRepository;
 use App\Repository\GoalSectionRepository;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use http\Env\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
- * @Route("/goal", name="goal_index")
+ * @Route("/goal")
  */
 class GoalController extends AbstractController
 {
     /**
      * @Route("/", name="goal_index")
-     * @param GoalRepository $goals
      * @param GoalSectionRepository $goalSectionRepository
      * @return Response
      */
-    public function index (GoalRepository $goals, GoalSectionRepository $goalSectionRepository) {
+    public function index (GoalSectionRepository $goalSectionRepository) {
 
+        $user = $this->getUser();
+        $userGoals = $this->getDoctrine()
+            ->getRepository(User::class)
+            ->findOneByUsername($this->getUser()->getUsername());
         return $this->render('goal/index.html.twig', [
-            'goals'=>$goals->findAll(),
+          'userGoals'=>$userGoals,
             'goalSectionRepository'=>$goalSectionRepository->findAll()
         ]);
+    }
+
+    /**
+     * @Route("/{id}/my-goal", name="goal_add", methods={"GET","POST"})
+     * @param Goal $goal
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
+    public function addToMyGoal(Goal $goal, EntityManagerInterface $manager): Response {
+        $this->getUser()->addGoal($goal);
+        $manager->flush();
+        return $this->redirectToRoute('goal_index');
     }
 }
