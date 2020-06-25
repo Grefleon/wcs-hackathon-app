f72c4acdc089623acd9ebdd6913b6a86c82fa518
Oleg Chenesseau <oleg.chenesseau@gmail.com>
2020-06-26 01:40:10 +0200

    Adding modifying interests option

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,7 @@ Encore
     .addStyleEntry('login', './assets/scss/login.scss')
     .addStyleEntry('main', './assets/scss/main.scss')
     .addStyleEntry('goal', './assets/scss/goal.scss')
+    .addStyleEntry('interests', './assets/scss/interests.scss')
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
 
